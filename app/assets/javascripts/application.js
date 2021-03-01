f23724e9de287d790f562ae6b2720dfb01fb2965
konstantinos <braindamnjam@gmail.com>
2021-03-01 09:36:01 +0000

    new

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,49 @@
+/* global $ */
+
+// Warn about using the kit in production
+if (window.console && window.console.info) {
+  window.console.info('GOV.UK Prototype Kit - do not use for production')
+}
+
+$(document).ready(function () {
+  window.GOVUKFrontend.initAll()
+})
+
+$( document ).ready(function() {
+
+
+
+
+    
+  $("#sexualOffenceYes").click(function(){
+    	$(".hiddenDate").show();
+	});
+
+	$("#sexualOffenceNo").click(function(){
+    	$(".hiddenDate").hide();
+	});
+
+
+
+
+  	
+
+
+
+	
+
+
+
+
+
+
+
+	
+
+
+
+
+
+
+
+});
