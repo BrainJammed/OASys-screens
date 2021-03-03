1695b31fd7ca99a8c2282108df85ccc794b92608
konstantinos <braindamnjam@gmail.com>
2021-03-03 14:55:47 +0000

    changes

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -5,9 +5,13 @@ if (window.console && window.console.info) {
   window.console.info('GOV.UK Prototype Kit - do not use for production')
 }
 
+
+
 $(document).ready(function () {
   window.GOVUKFrontend.initAll()
 })
+
+
 
 $( document ).ready(function() {
 
